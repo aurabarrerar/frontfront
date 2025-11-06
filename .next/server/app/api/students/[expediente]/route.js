ac473883c6333f151edd50fcc411bd4725bb0af4
Aurita <alejandritainuzuka@gmail.com>
2025-11-05 17:09:13 -0700

    Eliminar carpeta .next del repositorio

--- .next/server/app/api/students/[expediente]/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/students/[expediente]/route.js")
-R.c("server/chunks/[root-of-the-server]__41e4cc34._.js")
-R.c("server/chunks/[root-of-the-server]__c7eebac0._.js")
-R.m(58632)
-R.m(58637)
-module.exports=R.m(58637).exports
--- .next/static/chunks/084497c9e0ed53f0.js
+++
@@ -1,5 +0,0 @@
-__turbopack_load_page_chunks__("/_error", [
-  "static/chunks/17722e3ac4e00587.js",
-  "static/chunks/0026da97929cfdfd.js",
-  "static/chunks/turbopack-b40ba94a7ed88c60.js"
-])
